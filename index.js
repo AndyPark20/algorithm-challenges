f10bd2e98dd87342b89a9044e0ec1e5a2f519276
Andy Park <apark1214@gmail.com>
2021-03-04 08:12:06 -0800

    working on array challenges

--- index.js
+++ index.js
@@ -13,14 +13,44 @@ In other words, if there are more than 1 duplicated numbers,
 return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
 */
 
+// function firstDuplicate(a) {
+//   for (var i = 0; i < a.length; i++) {
+//     var num = a[i];
+//     if (a[Math.abs(num) - 1] > 0) {
+//       a[Math.abs(num) - 1] = -1 * a[Math.abs(num) - 1];
+//     }
+//     else {
+//       return Math.abs(num);
+//     }
+//   }
 
-function firstDuplicate(a) {
-  for (let i = 0; i < a.length; i++) {
-    for (let z = 0; z < a.length; z++) {
-      if (a[i] === a[z] && i > z) {
-        return a[i]
-      }
-    }
-  }
-  return -1
-}
+//   return -1;
+// }
+
+// console.log(firstDuplicate([2,4,5,2,3,6,8,3]))
+
+
+/*
+Share
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+
+var twoSum = function(nums, target) {
+  let array=[];
+ for(let i=0; i<nums.length;i++){
+   for (let z=0;z<nums.length;z++){
+     if(nums[i] + nums[z] === target && (nums[i] + nums[i] !== target)){
+      array.push(i, z)
+     }
+   }
+   return array;
+ }
+
+};
+
+console.log(twoSum([3,2,4],6))
