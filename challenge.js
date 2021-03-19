8a84bad52e2fd476f89f7ca18bf23911ed85a6e0
Andy Park <apark1214@gmail.com>
2021-03-18 21:21:49 -0700

    added anagram challenge

---
+++ challenge.js
@@ -0,0 +1,20 @@
+Anagrammy Collector
+provided an array of lowercase strings, return the array of similiar anagrams together
+
+function anagrammyCollector() {
+
+}
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+
+  [
+    ["ate", "eat", "tea"],
+    ["nat", "tan"],
+    ["bat"]
+  ]
+
+Output: 3
+
+["cat", "atc", "silent", "many"]
+[["cat", "atc"], ["many"], ["silent"]]
+Output 3
