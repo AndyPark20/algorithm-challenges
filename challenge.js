1a44736bbfffeb92054dcdab01aa8503e5c3b5d1
Andy Park <apark1214@gmail.com>
2021-03-19 15:00:02 -0700

    working on anagram

--- challenge.js
+++ challenge.js
@@ -1,20 +1,38 @@
-Anagrammy Collector
-provided an array of lowercase strings, return the array of similiar anagrams together
+// Anagrammy Collector
+// provided an array of lowercase strings, return the array of similiar anagrams together
 
-function anagrammyCollector() {
-
+function anagrammyCollector(arr) {
+  let sortArray=[];
+  let sameWord="";
+  let sameArray=[];
+  for (let i =0; i<arr.length; i++){
+    sortArray.push(arr[i].split('').sort().join(''))
+  }
+  for (let x=0; x<sortArray.length;x++){
+    sameWord+=sortArray[x]
+    if(sameWord === sortArray[x]){
+      sameArray.push([sortArray[x]])
+    }else{
+      sameArray.push([sortArray[x]])
+    }
+  }
+  console.log(sortArray)
+  console.log(sameArray)
 }
 
-Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+let array = ["eat", "tea", "tan", "ate", "nat", "bat"];
+anagrammyCollector(array)
 
-  [
-    ["ate", "eat", "tea"],
-    ["nat", "tan"],
-    ["bat"]
-  ]
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
 
-Output: 3
+//   [
+//     ["ate", "eat", "tea"],
+//     ["nat", "tan"],
+//     ["bat"]
+//   ]
 
-["cat", "atc", "silent", "many"]
-[["cat", "atc"], ["many"], ["silent"]]
-Output 3
+// Output: 3
+
+// ["cat", "atc", "silent", "many"]
+// [["cat", "atc"], ["many"], ["silent"]]
+// Output 3
