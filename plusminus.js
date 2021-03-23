de6b820276c93c0ad694ac61a8de7107b9aab845
Andy Park <apark1214@gmail.com>
2021-03-23 09:26:01 -0700

    rearranging min array

--- plusminus.js
+++ plusminus.js
@@ -5,7 +5,7 @@
 // Input is elements in an array with integer value (will Only count one value that has positive, negative, or zero)
 
 function plusMinus(arr){
-  //Create a variable and assign an empty object
+  //Create a variable and assign an object with neg, pos, and 0 as property with initial value of 0
   // Create a variable and assign an empty array
   // Create a variable and assign 0
   //loop thru the array and assign the value as property and assign value of 1
@@ -18,31 +18,25 @@ function plusMinus(arr){
 
 
 
-  let compareObj={};
+  let compareObj={'neg':0,'pos':0,'0':0};
   let finalArray=[];
   let number =0;
   for (let i=0; i<arr.length; i++){
-    compareObj['negative'] = 0;
-    compareObj['positive'] = 0;
-    compareObj['0'] = 0;
-    if(Math.sign(arr[i])===-1){
-      console.log('negative', arr[i])
-      compareObj['negative']++;
-    }else if(Math.sign(arr[i])===1){
-      console.log('positive', arr[i])
-      compareObj['positive']++;
-    }else if(Math.sign(arr[i])===0){
+    if(Math.sign(arr[i])=== -1){
+      compareObj['neg']++;
+    }else if(Math.sign(arr[i])=== 1){
+      compareObj['pos']++;
+    }else if(Math.sign(arr[i])=== 0){
       compareObj['0']++;
     }
   }
-  console.log(compareObj)
-  // for (const keys in compareObj){
-  //   let divisionResult= compareObj[keys] / arr.length;
-  //   let finalNumber = divisionResult.toFixed(6)
-  //   finalArray.push(finalNumber);
-  // }
-  // return finalArray;
+  for (const keys in compareObj){
+    let divisionResult= compareObj[keys] / arr.length;
+    let finalNumber = divisionResult.toFixed(6)
+    finalArray.push(finalNumber);
+  }
+  return finalArray;
 
 }
 
-console.log(plusMinus([-4, 3, -9, 0, 4, 1,-6]))
+console.log(plusMinus([0, 0, -1, 1, 1,]))
