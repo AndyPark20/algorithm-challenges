43c130301409e762a91b6729a9e78b2b2e8e7de0
Andy Park <apark1214@gmail.com>
2021-03-24 15:12:43 -0700

    birthday cake set up

---
+++ hacker-rank/birthday.js
@@ -0,0 +1,8 @@
+
+
+//OUTPUT:Integer that represents how many tallest candles are in the array;
+//INPUT: An array representing with elements that repsents unit height of each candle
+
+function birthday(candles){
+
+}
