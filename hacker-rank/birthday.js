c26396aed429540b8512638bdf0a9bc5173e6930
Andy Park <apark1214@gmail.com>
2021-03-24 16:18:06 -0700

    completed birtday cake using Math.max

--- hacker-rank/birthday.js
+++ hacker-rank/birthday.js
@@ -4,5 +4,20 @@
 //INPUT: An array representing with elements that repsents unit height of each candle
 
 function birthday(candles){
+    //Get the Max value from the array
+    //Loop thru the array and check to see if there is any element in the array that matches the max value,
+    //If it does, push it into an empty array.
+    //Use .length to determine how many values are in the array
+    //Return it
 
+  const array=[];
+  const maxNumber =Math.max(...candles);
+  for(let i =0;i<candles.length;i++){
+    if(maxNumber === candles[i]){
+      array.push(candles[i]);
+    }
+  }
+  console.log(array.length);
 }
+
+console.log(birthday([4,4,1,3]))
