dc59f09b165cf26f1b4e0aa8125c8733e345713c
Andy Park <apark1214@gmail.com>
2021-03-25 16:13:24 -0700

    adding progress

--- misc-challenge/challenge.js
+++ misc-challenge/challenge.js
@@ -1,28 +1,8 @@
 // Anagrammy Collector
 // provided an array of lowercase strings, return the array of similiar anagrams together
 
-// function anagrammyCollector(arr) {
-//   let sortedObject = {};
-//   let masterArray = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     const propValue = arr[i].split('').sort().join('');
-//     if (!sortedObject[propValue]) {
-//       sortedObject[propValue] = [arr[i]]
-//     } else {
-//       sortedObject[propValue].push(arr[i])
-//     }
-//   }
-//   for (const keys in sortedObject) {
-//     masterArray.push(sortedObject[keys])
-//   }
-//   return masterArray
-// }
-
-
-
 
 // Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-
 //   [
 //     ["ate", "eat", "tea"],
 //     ["nat", "tan"],
@@ -30,12 +10,11 @@
 //   ]
 
 // Output: 3
-
 // ["cat", "atc", "silent", "many"]
 // [["cat", "atc"], ["many"], ["silent"]]
 // Output 3
 
-function anagrammyCollector(arr){
+function anagrammyCollector(arr) {
   let someArray = {};
   let checkWord = ''
   for (let i = 0; i < arr.length; i++) {
