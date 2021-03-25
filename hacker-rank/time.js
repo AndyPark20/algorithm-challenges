a5a67f8bd5831b5fbaeec06a61ffd3cd8faff93c
Andy Park <apark1214@gmail.com>
2021-03-24 19:31:30 -0700

    re-solving military time

--- hacker-rank/time.js
+++ hacker-rank/time.js
@@ -14,28 +14,41 @@ function militaryTime(s){
   // use splice method to add and replace the current hour listed on the string.
 
   //taking the hour string and coverting it to military time
-  const stringConvert = s.split('');
-  const selectedElement =stringConvert.slice(0,2)
-  const joinSelectedElement =selectedElement.join('')
-  const convertToNumber = parseInt(joinSelectedElement)
-  let addNumber = convertToNumber + 12;
-  let numberToString =''
-  if(addNumber ===24){
-    numberToString='00';
-  }else{
-    numberToString= addNumber.toString();
-  }
-
-  //adding the converted military hour time to the rest of the string and also delete PM and AM
-  const mainElement= stringConvert.splice(2)
-  mainElement.unshift(numberToString);
-  console.log(mainElement)
-  const finalResult =mainElement.join('')
-  console.log(finalResult)
-
-
-
 
 }
 
-console.log(militaryTime('12:05:45PM'))
+
+
+
+// const stringConvert = s.split('');
+// const selectedElement = stringConvert.slice(0, 2)
+// const joinSelectedElement = selectedElement.join('')
+// const convertToNumber = parseInt(joinSelectedElement)
+// let addNumber = convertToNumber + 12;
+// let numberToString = ''
+// if (addNumber === 24) {
+//   numberToString = '00';
+// } else {
+//   numberToString = addNumber.toString();
+// }
+
+// //adding the converted military hour time to the rest of the string and also delete PM and AM
+// const mainElement = stringConvert.splice(2)
+// mainElement.unshift(numberToString);
+
+// //Looping thru array and finding P or A, get that index value and use splice to remove it from the array
+// let indexNumber = 0;
+// for (let i = 0; i < mainElement.length; i++) {
+//   if (mainElement[i] === 'P') {
+//     indexNumber = mainElement.indexOf('P');
+//   } else if (mainElement[i] === 'A') {
+//     indexNumber = mainElement.indexOf('A');
+//   }
+// }
+// // useIndexNumber to spliace
+// mainElement.splice(7, 2)
+// const finalResult = mainElement.join('')
+// console.log(finalResult)
+// }
+
+// console.log(militaryTime('08:05:45PM'))
