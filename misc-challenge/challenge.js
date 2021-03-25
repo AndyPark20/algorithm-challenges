1d6e97f2c347613178fdea9da0c9a79706603724
Andy Park <apark1214@gmail.com>
2021-03-25 14:38:50 -0700

    revising anagram medium challenge

--- misc-challenge/challenge.js
+++ misc-challenge/challenge.js
@@ -3,17 +3,21 @@
 
 function anagrammyCollector(arr) {
   let someArray={};
-  let checkWord=''
+  let checkWord='';
+  let masterArray=[];
   for (let i=0;i<arr.length;i++){
-    someArray[arr[i].split('').sort().join('')] = arr[i];
-  }
+    const propValue = arr[i].split('').sort().join('');
+    someArray[arr[i]] = propValue;
+    }
+
+  for (const keys in someArray){}
 
   console.log(someArray)
-    return Object.keys(someArray).length;
+
 
 }
 
-let array = ["cat", "atc", "silent", "many","miss","tac"];
+let array = ["eat", "tea", "tan", "ate", "nat", "bat"];
 console.log(anagrammyCollector(array));
 
 // Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
@@ -29,3 +33,11 @@ console.log(anagrammyCollector(array));
 // ["cat", "atc", "silent", "many"]
 // [["cat", "atc"], ["many"], ["silent"]]
 // Output 3
+
+//  let someArray={};
+// let checkWord = ''
+// for (let i = 0; i < arr.length; i++) {
+//   someArray[arr[i].split('').sort().join('')] = arr[i];
+// }
+
+// return Object.keys(someArray).length;
