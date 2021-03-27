1b12f95cb6af7b9c9f2815d0d99ae8fba33c8f93
Andy Park <apark1214@gmail.com>
2021-03-26 22:21:30 -0700

    written ouput and input of the fruit challenge

---
+++ hacker-rank/fruit.js
@@ -0,0 +1,16 @@
+//OUTPUT return integers how many apples(first) and oranges(second) fell in Sams house.
+//INPUT
+// first array (s and t)--> the distance of sams house (whatever falls in between counts)
+// second array (a and b) --> the position of which apple and orange tree is located
+// third array (m and n) -->how many apples and oranges have fallen
+// fourth array (m) --> the distance at which the apples have fallen
+// fifth array (n) --> the distance at which oranges have fallen
+
+
+
+function kangaroo (value1,value2){
+
+}
+
+
+console.log(kangaroo(3,4))
--- misc-challenge/challenge.js
+++ misc-challenge/challenge.js
@@ -1,8 +1,39 @@
 // Anagrammy Collector
 // provided an array of lowercase strings, return the array of similiar anagrams together
 
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+//   [
+//     ["ate", "eat", "tea"],
+//     ["nat", "tan"],
+//     ["bat"]
+//   ]
+// Output: 3
 
 // Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+// ["cat", "atc", "silent", "many"]
+// [["cat", "atc"], ["many"], ["silent"]]
+// Output 3
+
+// function anagrammyCollector(arr) {
+//   let sortedObject = {};
+//   let masterArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     const propValue = arr[i].split('').sort().join('');
+//     if (!sortedObject[propValue]) {
+//       sortedObject[propValue] = [arr[i]]
+//     } else {
+//       sortedObject[propValue].push(arr[i])
+//     }
+//   }
+//   for (const keys in sortedObject) {
+//     masterArray.push(sortedObject[keys])
+//   }
+//   return masterArray
+// }
+
+
+
+
 //   [
 //     ["ate", "eat", "tea"],
 //     ["nat", "tan"],
@@ -15,12 +46,30 @@
 // Output 3
 
 function anagrammyCollector(arr) {
-  let someArray = {};
-  let checkWord = ''
+  //Create a variable dictionary and assign an empty object
+  // Create a variable masterArray and assign an empty array
+  //loop thru the array and re-sort the elements at index by using split, sort and join method
+  //check to see if the sorted element is in the dictionary,
+  // if it is in there, then push the element at index into the object.
+  // if not in there, enclose the element at index in [] and assign it to the object.
+  //Loop thru the dictionary object and push the keys into masterArray;
+  // Return masterArray;
+
+  let dictionary = {};
+  let masterArray = [];
   for (let i = 0; i < arr.length; i++) {
-    someArray[arr[i].split('').sort().join('')] = arr[i];
+    const sortString = arr[i].split('').sort().join('')
+    if (!dictionary[sortString]) {
+      dictionary[sortString] = [arr[i]]
+    } else {
+      dictionary[sortString].push(arr[i])
+    }
   }
-  return Object.keys(someArray).length;
+  console.log(dictionary)
+  for (const keys in dictionary) {
+    masterArray.push(dictionary[keys]);
+  }
+  // return masterArray
 }
 
 let array = ["eat", "tea", "tan", "ate", "nat", "bat"];
