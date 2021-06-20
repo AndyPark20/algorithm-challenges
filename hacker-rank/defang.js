5d582dc2513ebc68f3ea0c167120ecc546c1e909
Andy Park <apark1214@gmail.com>
2021-06-19 22:17:44 -0700

    pseudo code completed

--- hacker-rank/defang.js
+++ hacker-rank/defang.js
@@ -9,3 +9,14 @@ Output: "1[.]1[.]1[.]1"
 Example 2:
 Input: address = "255.100.50.0"
 Output: "255[.]100[.]50[.]0"*/
+
+function defang(){
+  //Use the split method to push the string into array by ''
+  //loop thru the array
+  //if the index at value i is equal to .
+  //push the . to an array.
+  // if the index at value i is equal to 1
+  //push 1 to an empty string.
+
+
+}
