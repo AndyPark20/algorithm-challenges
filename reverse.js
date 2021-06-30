61c181821b58c319cd6f9d74fd088d43f01cd1e0
Andy Park <apark1214@gmail.com>
2021-06-29 20:49:11 -0700

    revising pseudo code

--- reverse.js
+++ reverse.js
@@ -26,14 +26,15 @@ var reverse = function (x) {
 //turn the number into strings
 //split the strings by each character and put them into an array
 //loop the array backwards and check if the values at index i is a number.
-  //if it is anything besides '-' then put it into a variable that is assigned as an empty string.
-  // if the value at index i is '-' then put the value at the first of the string
-  //turn the string into integers
-  //return integer
+  //if it is anything besides '-' then put it into a variable that is assigned as an empty array.
+  // if the value at index i is '-' then put the value at the first of the array
+//loop thru the new array that has the string values and place them into an empty string
+//return parseInt of the final string
 
 let convertString = x.toString();
 let splitString=convertString.split('');
 let checkedArray=[];
+let final =''
 for (let i=splitString.length;i>=0;i--){
   if(splitString[i] ==='-'){
     checkedArray[0]=splitString[i];
@@ -41,7 +42,11 @@ for (let i=splitString.length;i>=0;i--){
     checkedArray.push(splitString[i])
   }
 }
-return checkedArray
+for (let i=0; i<checkedArray.length;i++){
+  final+=checkedArray[i];
+
+}
+return parseInt(final)
 };
 
 let input = -123
