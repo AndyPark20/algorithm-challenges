2c36fa5467c0334538bdb341781b0a6c053162d9
Andy Park <apark1214@gmail.com>
2021-08-20 21:08:52 -0700

    in progress

--- palindrome.js
+++ palindrome.js
@@ -8,9 +8,26 @@ function palindrome (stringPalindrome){
       // if divisble by 2 and equals 0 ==> return true
       // else return false
 
-    for(let i =0; i<stringPalindrome.length;i++){
+    const hashmapPalindromeObject ={};
+    let count =1;
 
+
+    //creating hashmapObject
+    for(let i =0; i<stringPalindrome.length;i++){
+      if(!hashmapPalindromeObject[stringPalindrome[i]]){
+          hashmapPalindromeObject[stringPalindrome[i]] = count;
+      }else{
+        hashmapPalindromeObject[stringPalindrome[i]]++
+      }
     }
 
-
+    //Checking to see if the values from hashmapPalindromObject is divisble by 2 to make it equal 0
+    for(let keys in hashmapPalindromeObject){
+      if(hashmapPalindromeObject[keys] %2 ===0){
+        return true;
+      }
+      return false;
+    }
 }
+
+console.log(palindrome("aaa"))
