b7effc55bd36c0b23f845476710a7e92c2d518b4
Andy Park <apark1214@gmail.com>
2021-08-20 20:59:13 -0700

    starting palindrome exccersie

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+
+
+
+
+function palindrome (stringPalindrome){
+  //create a hashmapObject with the string character as key and the number of same characters as values
+    //divide the values via Object.value(object name) and divide it by 2
+      // if divisble by 2 and equals 0 ==> return true
+      // else return false
+
+    for(let i =0; i<stringPalindrome.length;i++){
+
+    }
+
+
+}
