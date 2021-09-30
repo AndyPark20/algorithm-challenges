b924d2898a92405817777826da085f7a070ca017
Andy Park <apark1214@gmail.com>
2021-09-29 21:56:16 -0700

    working on javascript math

---
+++ scratch.js
@@ -0,0 +1,13 @@
+
+let numOne = 2;
+let numTwo =4;
+
+
+function remainder(a,b){
+  if(b % a ===0){
+    return 'its an even number'
+  }
+  return 'Its an odd number'
+}
+
+console.log(remainder(numOne,numTwo));
