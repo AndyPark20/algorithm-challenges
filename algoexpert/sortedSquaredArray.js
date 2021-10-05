3ef13032ef1ba4c631746a5c8aea2b5ea3ff56d2
Andy Park <apark1214@gmail.com>
2021-10-04 21:45:33 -0700

    evaluating initial attempted on squared array

--- algoexpert/sortedSquaredArray.js
+++ algoexpert/sortedSquaredArray.js
@@ -2,10 +2,18 @@
 
 function sortedSquaredArray(array) {
   // Write your code here.
-  // Create a variable called sortedArray and assign an empty array;
+  // Create a variable called squaredArray and assign an empty array;
   //Use the sort() initially, to sort the array in ascending order
     // Loop thru the sorted array
       //Use pow method to square each values at index of the array.
-      //push the squared value into the sortedarray variable and return it.
-  return [];
+      //push the squared value into the squaredArray variable and return it.
+
+  let sortedArray = array.sort();
+  let squaredArray=[];
+  for (i=0;i<sortedArray.length;i++){
+    squaredArray.push(Math.pow(sortedArray[i],2))
+  }
+  return squaredArray;
 }
+
+console.log(sortedSquaredArray([4,1,2,19,-1]))
