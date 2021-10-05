30cfed7e335c6ecd043ab630c405a43a84a1a760
Andy Park <apark1214@gmail.com>
2021-10-04 20:55:40 -0700

    in progress

--- algoexpert/validateSequence.js
+++ algoexpert/validateSequence.js
@@ -3,11 +3,13 @@
 
 function isvalidSubsequence(array, sequence){
   //Loop thru the array
-  //create a variable j that will be count the index of the sequence array
+  //create a variable seqIdx that will be count the index of the sequence array
     //if the value at index array matches the value at index of sequence array
       //then increment variable j
     //if the value of variable j is equalto the length of the array
       //return true;
     //else
       //return false;
+  let seqIdx=0;
+
 }
