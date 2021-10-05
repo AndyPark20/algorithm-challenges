fc48ced4ae03f32b4245ca55d4d3abc24a91156e
Andy Park <apark1214@gmail.com>
2021-10-04 21:41:09 -0700

    working on squared array

--- algoexpert/sortedSquaredArray.js
+++ algoexpert/sortedSquaredArray.js
@@ -1,1 +1,11 @@
-console.log('gello')
+
+
+function sortedSquaredArray(array) {
+  // Write your code here.
+  // Create a variable called sortedArray and assign an empty array;
+  //Use the sort() initially, to sort the array in ascending order
+    // Loop thru the sorted array
+      //Use pow method to square each values at index of the array.
+      //push the squared value into the sortedarray variable and return it.
+  return [];
+}
