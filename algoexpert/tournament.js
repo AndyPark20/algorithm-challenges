ba5d27281450fac525fe6492737166d08ea383c2
Andy Park <apark1214@gmail.com>
2021-10-05 19:23:40 -0700

    in progress

--- algoexpert/tournament.js
+++ algoexpert/tournament.js
@@ -8,12 +8,22 @@ function tournamentWinner(competitions, results) {
     // 1---> home team won
 
 
-  //create a variable for the team that won the series
+  //create a variable that will record each team score
   // create a variable that will increment to loop competitions and results array and start at 0
   //at that index use for loop to loop thru the array at index.
     // If the value at index of results is 0, then add index + 1, and use this index to determine the name of the away team in competitions array
     // if the value at index of results is 1, then index -1, and use this index to determine the name of the home team
 
+    let scoreRecord ={};
+    let winner ='';
+    let x =0;
+    for (let i=0;i<competitions.length;i++){
+      if(results[x]===0){
+       winner= competitions[i][results[x]+1]
+      }
+    }
+
+    console.log(winner)
 
 
 
