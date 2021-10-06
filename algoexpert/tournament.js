7fc7cd2e1bee4c0dacd0d90c5bdc202078627c70
Andy Park <apark1214@gmail.com>
2021-10-05 19:00:31 -0700

    in progress for pseudo code

--- algoexpert/tournament.js
+++ algoexpert/tournament.js
@@ -1,6 +1,17 @@
 
 
 function tournamentWinner(competitions, results) {
-  // Write your code here.
+  //facts
+    //array 1 for competitiors --> [home, away];
+    //array 2 has the competition results for each game
+    // 0--> away team won
+    // 1---> home team won
+
+
+  //create a variable for the team that won the series
+
+
+
+
   return '';
 }
