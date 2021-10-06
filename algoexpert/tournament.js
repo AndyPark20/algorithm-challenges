556a9bcec8b58b50c2f4900b3b96c550feee99ed
Andy Park <apark1214@gmail.com>
2021-10-05 20:03:23 -0700

    working on nonConstructable algo

--- algoexpert/tournament.js
+++ algoexpert/tournament.js
@@ -1,37 +1,26 @@
 
 
 function tournamentWinner(competitions, results) {
-  //facts
-    //array 1 for competitiors --> [home, away];
-    //array 2 has the competition results for each game
-    // 0--> away team won
-    // 1---> home team won
-
-  //create a variable that will record each team score
-  // create a variable that will increment to loop competitions and results array and start at 0
-  //at that index use for loop to loop thru the array at index.
-    // If the value at index of results is 0, then add index + 1, and use this index to determine the name of the away team in competitions array
-    // if the value at index of results is 1, then index -1, and use this index to determine the name of the home team
 
     let scoreRecord ={};
-    let winner ='';
+    let roundWinner ='';
     let x =0;
     for (let i=0;i<competitions.length;i++){
       if(results[x]===0){
-       winner= competitions[i][results[x]+1]
+       roundWinner= competitions[i][results[x]+1]
        x++;
       }else {
-        winner = competitions[i][results[x]-1];
+        roundWinner = competitions[i][results[x]-1];
         x++
       }
-      if(!scoreRecord[winner]){
-        scoreRecord[winner] =1;
+      if(!scoreRecord[roundWinner]){
+        scoreRecord[roundWinner] =1;
       }else{
-        scoreRecord[winner]++
+        scoreRecord[roundWinner]++
       }
     }
 
-    let finalWinner =Object.keys(scoreRecord).reduce((a,b)=>scoreRecord[a]> scoreRecord[b] ?a : b);
+    finalWinner=Object.keys(scoreRecord).reduce((a,b)=>scoreRecord[a]> scoreRecord[b] ?a : b);
 
     return finalWinner;
 
