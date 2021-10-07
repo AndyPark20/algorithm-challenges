0829872a4e4b9fc3ea0377fc1c47ab94946488a8
Andy Park <apark1214@gmail.com>
2021-10-06 21:04:10 -0700

    in progress

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -2,20 +2,21 @@ var twoSum = function (nums, target) {
   //create a variable that will store the smallest value in the array
     //find the smallest number in the nums array and assign it to the variable we created
     // Loop thru the nums array, and add each value (except the smallest value) to the variable and check if it matches the target number
-    let twoSumArray =[];
     let smallestNumber = Math.min(...nums);
     let smallestNumberIndex = nums.indexOf(smallestNumber);
+    let twosumsArray=[smallestNumberIndex];
 
     for (let i=0;i<nums.length;i++){
-      if((nums[i] + smallestNumber===target) &&)
+      if((nums[i] + smallestNumber===target) && (i!==smallestNumberIndex)){
+          twosumsArray.push(i)
+      }
     }
-
-console.log(smallestNumber)
-
+    return twosumsArray;
 };
 
-let nums = [2, 7, 11, 15]
-let target = 9;
+let nums = [3, 2, 3]
+
+let target = 6;
 
 
 console.log(twoSum(nums,target))
