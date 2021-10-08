825201748c3b62b9058b1266fb11ded95660a5ad
Andy Park <apark1214@gmail.com>
2021-10-07 21:11:47 -0700

    finished writing psudo code

--- leetcode/palindrone.js
+++ leetcode/palindrone.js
@@ -1,5 +1,9 @@
 
 
-function checkPalindrome(arr){
-
+function checkPalindrome(num){
+  //turn num into string and put it into an array;
+    //make a copy of the array and reverse it
+    //loop thru the first array, and if the value at index of the first array matches the value at index of second
+    //array, then keep looping.
+      //if at anypoint its different, return false
 }
