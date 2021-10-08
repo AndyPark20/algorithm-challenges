64a775794adb206bfc2eb5f38f4fe32d6dd5f5c2
Andy Park <apark1214@gmail.com>
2021-10-07 22:27:48 -0700

    in progress

--- leetcode/romanNumeral.js
+++ leetcode/romanNumeral.js
@@ -14,20 +14,15 @@ let romanNumerals={
   //create a variable called romanNumbers and store the converted string into array;
   //loop thru romanNumbers, and check if the value at index is a property in the romanNumerals object;
   //if it is a property, then add the number into numbers variable.
-  let specialNumbers ={
-    5:1,
-    10:1,
-    50:1,
-    100:1,
-    500:1,
-    1000:1
-  }
+
 
   let numbers =0;
   let romanNumbers =[...string];
   for (let i=0;i<romanNumbers.length;i++){
-    if(specialNumbers.hasOwnProperty(romanNumbers[i]))){
+    if(romanNumbers.length>2){
       numbers+=romanNumerals[romanNumbers[i]];
+    }else{
+      if(romanNumbers[i])
     }
   }
   return numbers;
