30addce3bcf6c52d91147c3c9ac0af7800f2a68a
Andy Park <apark1214@gmail.com>
2021-10-07 22:08:06 -0700

    completed writing psuedo code

--- leetcode/romanNumeral.js
+++ leetcode/romanNumeral.js
@@ -1,5 +1,20 @@
 
 
-function guessNumber(){
+function guessNumber(string) {
+let romanNumerals={
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+}
+  //Create a variable called numbers that will keep count as we iterate thru converted array;
+  //create a variable called romanNumbers and store the converted string into array;
+  //loop thru romanNumbers, and check if the value at index is a property in the romanNumerals object;
+  //if it is a property, then add the number into numbers variable.
 
+  let numbers =0;
+  let romanNumbers =
 }
