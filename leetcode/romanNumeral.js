ec948fbc2e340ca15c7a47bbd3f4741f37fef90d
Andy Park <apark1214@gmail.com>
2021-10-10 20:55:16 -0700

    in progress

--- leetcode/romanNumeral.js
+++ leetcode/romanNumeral.js
@@ -19,12 +19,23 @@ let romanNumerals={
   let numbers =0;
   let romanNumbers =[...string];
   for (let i=0;i<romanNumbers.length;i++){
-    if((romanNumbers.length===2)&& (romanNumbers[i]==='I' && (romanNumbers[i+1]==='V' || romanNumbers[i+1]==='X'))){
-      numbers=romanNumerals[romanNumbers[i+1]]- romanNumerals[romanNumbers[i]]
-    }
+      if((romanNumbers[i] === 'I' && (romanNumbers[i + 1] === 'V' || romanNumbers[i + 1] === 'X'))){
+         numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
+         i++;
+      } else if ((romanNumbers[i] === 'X' && (romanNumbers[i + 1] === 'L' || romanNumbers[i + 1] === 'C'))){
+        numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
+        i ++;
+      } else if ((romanNumbers[i] === 'C' && (romanNumbers[i + 1] === 'D' || romanNumbers[i + 1] === 'M'))){
+       numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
+         i++;
+      }else if(!romanNumbers[i]){
+        numbers;
+      }else{
+        numbers+=romanNumerals[romanNumbers[i]]
+      }
   }
   return numbers;
 }
-s = "IX"
+s = "MCMXCIV"
 
 console.log(guessNumber(s))
