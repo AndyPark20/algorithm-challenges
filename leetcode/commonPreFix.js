6cd372c86a859b9c50514b16d6ee60ba1a016b54
Andy Park <apark1214@gmail.com>
2021-10-12 20:16:33 -0700

    reworking longest pre-fix sequence

--- leetcode/commonPreFix.js
+++ leetcode/commonPreFix.js
@@ -3,35 +3,18 @@
 
 
 var longestCommonPrefix = function (strs) {
-  //create a variable that will have contain the first value string in the array
-  //create another variable that will hold same characters found in each value of the array.
-    //Loop thru the variable and also loop thru the array
-      //if(the value at index of the second array is same as the value at the index of the original string)
-        //add it to the variable that holds the same character string
-      //if its not there, then loop out of the array
 
-      let valueTocompare =strs[0];
-      let samePreFix =[];
-      let finalPreFix='';
-      let count =0;
-      for (let i=0;i<valueTocompare.length;i++){
-        for(let z=1;z<strs.length;z++){
-          if(strs[z].indexOf(valueTocompare[i])>-1){
-            samePreFix.push(valueTocompare[i])
-          }else{
-            break;
-          }
-        }
-      }
-     for(let h=0;h<samePreFix.length;h++){
-      if(samePreFix[h]===valueTocompare[count]){
-        finalPreFix+=valueTocompare[count];
-        count++;
-      }
-     }
-     return finalPreFix;
+  let preFix =strs[0];
+  let preFixCharacter ="";
+  let preFixCharacterIndex=null;
+
+  for (let i=0 ;i<preFix.length;i++){
+    for (let z=1; z<strs.length;z++){
+      if(strs[z].indexOf(preFix[i]))
+    }
+  }
 };
 
-let strs = ["cig","cag"]
+let strs = ["flower","flooed","flight"]
 
 console.log(longestCommonPrefix(strs))
