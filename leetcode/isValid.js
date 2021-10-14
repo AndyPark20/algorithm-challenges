d3c722798b7b903ddd63c13eda943a9e91001a8c
Andy Park <apark1214@gmail.com>
2021-10-13 21:00:30 -0700

    writing code

--- leetcode/isValid.js
+++ leetcode/isValid.js
@@ -1,3 +1,21 @@
-function isValid(){
+function isValid(s){
+  //create a variable to store string that is converted into an array
+    //loop thru the array
+      //if the index is even and equals to the opening characters and if the index of odd is matching characters
+        //return true
+      // else return false;
+  let hashMapObj={
+    '{':'}',
+    '[':']',
+    '(':')'
+  };
 
+  let stack =[];
+  for (let i=0;i<s.length;i++){
+    if(hashMapObj.hasOwnProperty(s[i])){
+      stack.push(s[i]);
+    }
+  }
 }
+
+let s =['[',']','{','{']
