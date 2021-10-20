1274985b87bcca4abd68c2777747ab9e7a59ecfe
Andy Park <apark1214@gmail.com>
2021-10-19 22:08:58 -0700

    merge sorted list

--- leetcode/mergeTwoSortedList.js
+++ leetcode/mergeTwoSortedList.js
@@ -3,11 +3,14 @@
 var mergeTwoLists = function (l1, l2) {
   //use the concat method to merge the two array
     // Use the sort method to sort from smallest to largest number
-  let copiedArray = l1.concat();
-  let combinedArray = copiedArray.push(...l2);
-  let sortedArray = combinedArray.sort((a,b)=>a-b);
-  return sortedArray;
+  let copiedArray=li.append(l2)
+  return copiedArray.sort((a,b)=>a-b)
+
+
 };
 
 let arr1 =[1,2,3,4];
-let arr2 = [1.5.6.7]
+let arr2 = [1,5,6,7]
+
+
+console.log(mergeTwoLists(arr1,arr2))
