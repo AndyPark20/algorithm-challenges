e16b4c1eab4392e1631bc9a0d38a16c6b921159f
Andy Park <apark1214@gmail.com>
2021-10-21 11:08:33 -0700

    in progress

--- leetcode/mergeSorted.js
+++ leetcode/mergeSorted.js
@@ -1,5 +1,17 @@
 var merge = function (nums1, m, nums2, n) {
+//check if the number for m and n is greater than 0,
+  //if the number is not greater than 0, return empty array
+//looop thru each nums1 and nums2, and if the value at each index is greater than 0, then push the number in the
+//empty array that we created.
+  if(m ===0 && n===0){
+    return [];
+  }
+  for (let i=0;i<nums1.length;i++){
 
+  }
 };
 
-nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
+var nums1 = [1, 2, 3, 0, 0, 0]
+var m = 3
+var nums2 = [2, 5, 6]
+var n = 3
