cc8b041198ba89cf0595d39c742cb5df7b59993b
Andy Park <apark1214@gmail.com>
2021-10-26 21:04:16 -0700

    in progress

--- leetcode/mergeSorted.js
+++ leetcode/mergeSorted.js
@@ -1,23 +1,12 @@
 var merge = function (nums1, m, nums2, n) {
 
-let array=[];
-  for (let i=0;i<m;i++){
-    array.push(nums1[i])
-  }
+  nums1.push(...nums2);
 
-  for (let i=0;i<n;i++){
-    array.push(nums2[i])
-  }
-
-  array.sort((a,b)=>a-b);
-  return array;
 };
 
-var nums1 =[1, 2, 3, 0, 0, 0]
-var m=3
-var nums2 =[2, 5, 6]
-var n=3
-
-
+var nums1= [1, 2, 3, 0, 0, 0]
+var m= 3
+var nums2=[2, 5, 6]
+var n =3
 
 console.log(merge(nums1,m,nums2,n))
