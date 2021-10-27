6b94bd0bcd9177e23771548a30a00432011346e6
Andy Park <apark1214@gmail.com>
2021-10-26 20:52:46 -0700

    reworking on merge sort challenge

--- leetcode/mergeSorted.js
+++ leetcode/mergeSorted.js
@@ -1,25 +1,16 @@
 var merge = function (nums1, m, nums2, n) {
-//check if the number for m and n is greater than 0,
-  //if the number is not greater than 0, return empty array
-//looop thru each nums1 and nums2, and if the value at each index is greater than 0, then push the number in the
-//empty array that we created.
-let array =[];
-for (let i=0;i<nums1.length;i++){
-  if(nums1[i]===0){
-    nums1.splice(i)
-    array =nums1;
-  }
-}
 
-for (let i=0;i<nums2.length;i++){
-  if(nums2[i]!==0){
+let array=[];
+  for (let i=0;i<m;i++){
+    array.push(nums1[i])
+  }
+
+  for (let i=0;i<n;i++){
     array.push(nums2[i])
   }
 
-}
-
-return array.sort((a,b)=>a-b)
-
+  array.sort((a,b)=>a-b);
+  return array;
 };
 
 var nums1 =[1, 2, 3, 0, 0, 0]
