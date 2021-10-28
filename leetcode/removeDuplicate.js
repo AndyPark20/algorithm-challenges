fb4744182a155a3c71df5fa248d7d94db2a76de1
Andy Park <apark1214@gmail.com>
2021-10-27 22:38:59 -0700

    using filter to find out repeated index

---
+++ leetcode/removeDuplicate.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function (nums) {
+  nums.filter((value,index)=>{
+    console.log(index)
+  })
+};
+
+var nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+console.log(removeDuplicates(nums))
