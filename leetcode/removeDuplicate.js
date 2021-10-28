da4035a1f7d8d21d13378f5f4754a36b9e6f9d58
Andy Park <apark1214@gmail.com>
2021-10-27 23:05:13 -0700

    partial credit

--- leetcode/removeDuplicate.js
+++ leetcode/removeDuplicate.js
@@ -1,6 +1,12 @@
 var removeDuplicates = function (nums) {
-  const filtered= nums.filter((value,index)=>index !== nums.indexOf(value))
-  console.log(filtered)
+
+  for(let i=0;i<nums.length;i++){
+        if(nums[i]=== nums[i+1]){
+      nums.splice(i+1,1);
+      removeDuplicates(nums);
+    }
+  }
+  return nums.length;
 };
 
 var nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
