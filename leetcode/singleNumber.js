f81060417dd9a556eeba3f376b2c55dd0ad17d2b
Andy Park <apark1214@gmail.com>
2021-11-03 22:02:56 -0700

    completed pseduo code

--- leetcode/singleNumber.js
+++ leetcode/singleNumber.js
@@ -1,3 +1,10 @@
 var singleNumber = function (nums) {
+  //if the length of the array is 1, then return the value in the array;
+    //if the length is NOT 1, use indexOf to see if the value of the index of the array is present.
+      //if it is, then keep looping
+      //if not return that value in the array;
 
 };
+
+
+var nums = [2, 2, 1]
