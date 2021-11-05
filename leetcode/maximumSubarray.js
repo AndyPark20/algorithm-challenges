4d6691a9282307872aeb865db925125486aa0a9b
Andy Park <apark1214@gmail.com>
2021-11-04 20:53:27 -0700

    psudo code completed

---
+++ leetcode/maximumSubarray.js
@@ -0,0 +1,7 @@
+var maxSubArray = function (nums) {
+  //sort the array from smallest to largest number
+    //loop the sorted array, and use the indexOf to find any dubplicates
+      //if duplicates are found then add it to a new array;
+      //loop thru the new array and add the values in the array
+
+};
