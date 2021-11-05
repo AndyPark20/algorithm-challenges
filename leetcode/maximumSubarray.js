53695211042d3fcb6bae8ea23aa8634ab1e6d9bb
Andy Park <apark1214@gmail.com>
2021-11-04 20:57:22 -0700

    in progress

--- leetcode/maximumSubarray.js
+++ leetcode/maximumSubarray.js
@@ -2,6 +2,13 @@ var maxSubArray = function (nums) {
   //sort the array from smallest to largest number
     //loop the sorted array, and use the indexOf to find any dubplicates
       //if duplicates are found then add it to a new array;
-      //loop thru the new array and add the values in the array
+       //loop thru the new array and add the values in the array
 
+    const sortedArray = nums.sort((a,b)=>a-b)
+  return sortedArray
 };
+
+
+var nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+console.log(maxSubArray(nums))
