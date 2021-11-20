b6b2383160aacdbe56a563104315658edef061c7
Andy Park <apark1214@gmail.com>
2021-11-19 22:31:58 -0800

    completed psudo code

---
+++ leetcode/plusOne.js
@@ -0,0 +1,9 @@
+
+
+
+var plusOne = function(digits){
+  //loop thru the array and add each value at index into an empty string.
+    //convert the string into number.
+    //Add one
+    //create a new array containing all the digits as value at index.
+}
