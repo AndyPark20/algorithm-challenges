fe338c5a7c81f1726b7ce1b815f518da9b1b060d
Andy Park <apark1214@gmail.com>
2021-11-19 22:23:34 -0800

    working on plusOne algo

--- pre-algebra/practice.js
+++ pre-algebra/practice.js
@@ -1,20 +1,11 @@
 
+function arrayReverse(arr){
 
-//Find the smallest and largest integers withoiut using any javascript built in methods
+  let reversedArray=[];
 
-let arr =[1,2,4,5,6,7,8,10,11,12,13];
-let initial = arr[0];
-let smallIntial =null;
-
-function findMinMax(arr){
-  for (let i=0;i<arr.length;i++){
-    if(arr[i] >initial){
-      initial =arr[i]
-    }else{
-      smallIntial = arr[i];
-    }
+  for(let i=arr.length;i>0;i--){
+    reversedArray.push(arr[i])
   }
-  return `${initial}, ${smallIntial}`
-}
 
-console.log(findMinMax(arr))
+  return reversedArray;
+}
