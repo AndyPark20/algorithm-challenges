cfc2adfb79a701bf3be11e5ce6cd2527d540fa93
Andy Park <apark1214@gmail.com>
2021-11-19 23:18:06 -0800

    completed

--- leetcode/plusOne.js
+++ leetcode/plusOne.js
@@ -1,26 +1,18 @@
-
-
-
-var plusOne = function(digits){
+var plusOne = function (digits) {
   //loop thru the array and add each value at index into an empty string.
-    //convert the string into number.
-    //Add one
-    //create a new array containing all the digits as value at index.
+  //convert the string into number.
+  //Add one
+  //create a new array containing all the digits as value at index.
 
-    let stringNumber ='';
-    let finalArray=[];
-    for (let i=0;i<digits.length;i++){
-      stringNumber+=digits[i]
-
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (++digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
     }
-    console.log(BigInt(stringNumber))
-    let addOneFinalNumber = (Number(stringNumber) + 1).toString();
+  }
+  digits.unshift(1);
+  return digits;
+};
 
-    for (let k=0;k<addOneFinalNumber.length;k++){
-      finalArray.push(Number(addOneFinalNumber[k]));
-    }
-    return finalArray;
-}
-
-
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+console.log(plusOne([9, 9, 9, 9, 9]));
