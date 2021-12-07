3cc4febcd67e0c232d1235ae12c85f2b68477e2e
Andy Park <apark1214@gmail.com>
2021-12-06 16:09:31 -0800

    completed psuedo code?

---
+++ leetcode/removeelement.js
@@ -0,0 +1,9 @@
+var removeElement = function (nums, val) {
+//Loop thru the nums array,
+  //if the value at index i is equal to val, then remove that value in the array
+  // increment counter variable to keep track how many values were deleted
+  //subtract the length of the nums array with the increment counter variable
+  //return the counter variable number;
+
+
+};
