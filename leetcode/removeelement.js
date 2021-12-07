7cc24dd5818172ca1537085855e123d54eff37a7
Andy Park <apark1214@gmail.com>
2021-12-06 16:22:00 -0800

    completed

--- leetcode/removeelement.js
+++ leetcode/removeelement.js
@@ -4,6 +4,13 @@ var removeElement = function (nums, val) {
   // increment counter variable to keep track how many values were deleted
   //subtract the length of the nums array with the increment counter variable
   //return the counter variable number;
-
-
+  for (let i=0;i<nums.length;i++){
+    if(nums[i]===val){
+      nums.splice(i,1);
+      i--;
+    }
+  }
+  return nums.length
 };
+
+console.log(removeElement([3, 2, 2, 3], 3));
