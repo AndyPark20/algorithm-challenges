6c154dfe591d6772bd6f0502e421ba8c9b439833
AndyPark20 <apark1214@gmail.com>
2022-06-18 05:32:05 +0000

    combining number

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -1,7 +1,18 @@
 
 function addTwoNumbers(arr1,arr2){
-  var convertArr1 = arr1.toString()
-  console.log(convertArr1)
+  let convertedArr1 ='';
+  let convertedArr2 ='';
+
+  for (let i=0;i<arr1.length;i++){
+    convertedArr1+=arr1[i];
+  }
+
+  for (let z=0;z<arr2.length;z++){
+    convertedArr2+=arr2[z];
+  }
+  let combinedNumber=parseInt(convertedArr1) + parseInt(convertedArr2);
+  console.log(combinedNumber)
+
 }
 
 addTwoNumbers([1,2,3],[4,8,9])
