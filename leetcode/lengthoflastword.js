d222b94343cb9dfd5a6d45130fb9fe5cd1040d5a
Andy Park <apark1214@gmail.com>
2022-06-27 09:36:32 -0700

    psudo coding

--- leetcode/lengthoflastword.js
+++ leetcode/lengthoflastword.js
@@ -1,1 +1,10 @@
-console.log('hello')
+//split the characters into array
+// loop thru the array
+// if the index of the valid word is larger than previous than find the length of the larger word
+
+function lengthOfLastWord(arg){
+  let array = arg.split(" ");
+  console.log(array);
+}
+
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
