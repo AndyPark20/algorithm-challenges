4f40d06ea575cf0558f678cead60143a1f451d61
Andy Park <apark1214@gmail.com>
2022-06-27 09:39:31 -0700

    in progress

--- leetcode/lengthoflastword.js
+++ leetcode/lengthoflastword.js
@@ -2,9 +2,11 @@
 // loop thru the array
 // if the index of the valid word is larger than previous than find the length of the larger word
 
-function lengthOfLastWord(arg){
-  let array = arg.split(" ");
-  console.log(array);
-}
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "))
+var lengthOfLastWord = function (s) {
+  let convertArray = s.split('')
+  let count =0;
+  for (let i=0;i<convertArray.length;i++){
+
+  }
+};
