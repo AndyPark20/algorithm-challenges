67d01855b0bc12de609eb65a4015efafb5133956
Andy Park <apark1214@gmail.com>
2022-06-28 08:00:59 -0700

    psudo code completed

--- leetcode/sortList.js
+++ leetcode/sortList.js
@@ -1,4 +1,11 @@
 
+//Loop thru the array
+//assign the values at index into a variable
+//if the next value at index is the same as the value stored in the variable
+    //remove the value at index
+//return the new array
+
+
 var deleteDuplicates = function(head){
 
 }
