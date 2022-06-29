b3d0e3cdff99375d97fcd1f354ffb9fce1dd95a6
Andy Park <apark1214@gmail.com>
2022-06-29 07:35:35 -0700

    psudo code

--- leetcode/palindromeTwo.js
+++ leetcode/palindromeTwo.js
@@ -1,1 +1,9 @@
-console.log('hello')
+
+
+//Create a variable to add combine characters without spaces and commas
+  //reverse the string to see if they match the un-reversed string
+
+
+var isPalindrome = function(s) {
+
+};
