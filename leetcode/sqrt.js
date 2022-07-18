f6013030c5324f172c818ae8e2c81ce4c957b908
Andy Park <apark1214@gmail.com>
2022-07-18 09:15:59 -0700

    checking solution:

--- leetcode/sqrt.js
+++ leetcode/sqrt.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+var mySqrt = function (x) {
+  let sqrtNumber = Math.sqrt(x);
+  let truncatedNumber = Math.trunc(sqrtNumber);
+  return truncatedNumber;
+};
+
+console.log(mySqrt(8))
