ea11cdbebba262dffa1e9b9551ea1c46ebd4cfa8
Andy Park <apark1214@gmail.com>
2022-07-22 08:28:18 -0700

    in progress

--- leetcode/duplicate.js
+++ leetcode/duplicate.js
@@ -2,4 +2,14 @@
 //assign value at index to a variable
 // check to see if that value matches in the array besides the same index
 
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  let checkNumber =null;
+  let index= null;
+
+  for (let i=0;i<nums.length;i++){
+    checkNumber = nums[i];
+    index = i;
+    if(i !==)
+  }
+
+};
