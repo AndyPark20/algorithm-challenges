3196d95a6a6067e255d2675d3a26b5ca836324eb
Andy Park <apark1214@gmail.com>
2022-08-11 16:33:24 -0700

    partial complete

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -1,8 +1,16 @@
 
 
 
-function containsDuplicate(){
-  console.log('fire off!')
+function containsDuplicate(nums){
+    let baseNumber =nums[0];
+    let finalResponse = undefined;
+    for (let i=0;i<nums.length;i++){
+      while(baseNumber !==nums[i]){
+        finalResponse = false;
+      }
+      finalResponse=true;
+    }
+    return finalResponse;
 }
 
 
