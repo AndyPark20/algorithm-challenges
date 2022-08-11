c7f4bd5feb57218e2fec01dbb2501998d13b5588
Andy Park <apark1214@gmail.com>
2022-08-10 18:11:40 -0700

    in progress

---
+++ leetcode/longestCommonPrefix.js
@@ -0,0 +1,20 @@
+
+//create an empty object to store the letters of each word in array
+//loop thru the main array
+  //loop thru inner array
+      //if the character is not found in the empty object go ahead and assign it as key value pair
+//loop thru the object and if there are characters with the same number as the length of the original array, populate the letters
+
+
+
+const test = 'flower';
+
+console.log(test.lastIndexOf('flow'))
+
+
+
+
+//is the value at index 0 found in index 1?
+//if so at what index of the string does it end?
+//take that and iterate to the next word and is that value found in the last index?
+// if so up to what index?
