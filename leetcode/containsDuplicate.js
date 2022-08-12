010ced93283ee4e1b23f51cce925b81d2588b66f
Andy Park <apark1214@gmail.com>
2022-08-11 17:40:34 -0700

    in progress

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -1,27 +1,33 @@
-function containsDuplicate(nums) {
+// function containsDuplicate(nums) {
 
-/****SOLUTION #1 */
-//   let emptyObject = {};
-//   for (let i = 0; i < nums.length; i++) {
-//     if (!emptyObject[nums[i]]) {
-//       emptyObject[nums[i]] = 0;
+// /****SOLUTION #1 ****/
+// //   let emptyObject = {};
+// //   for (let i = 0; i < nums.length; i++) {
+// //     if (!emptyObject[nums[i]]) {
+// //       emptyObject[nums[i]] = 0;
+// //     }
+// //     emptyObject[nums[i]]++;
+// //   }
+
+// //  for(keys in emptyObject){
+// //    if(emptyObject[keys] >1){
+// //      return true;
+// //    }
+// //  }
+// //  return false;
+
+// /****###END OF SOLUTION1 ###****/
+
+// /****SOLUTION #2 ****/
+//   const filterValue = nums.filter((value, index, array)=>{
+//     if(value === array[index+1]){
+//       return true;
 //     }
-//     emptyObject[nums[i]]++;
-//   }
+//   })
+//         return false;
 
-//  for(keys in emptyObject){
-//    if(emptyObject[keys] >1){
-//      return true;
-//    }
-//  }
-//  return false;
+// }
 
-/****SOLUTION #2 */
- const findDuplicate = nums.reduce((previousValue,currentValue)=>{
+// let arr = [1,2,2,3,4];
 
- })
-}
-
-let arr = [1,2,3,4];
-
-console.log(containsDuplicate(arr));
+// console.log(containsDuplicate(arr));
