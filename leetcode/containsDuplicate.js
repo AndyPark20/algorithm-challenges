34f09e3b86606fe7d98545b7e31f293a2a282b32
Andy Park <apark1214@gmail.com>
2022-08-15 10:57:34 -0700

    workikng on moveZeros

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -1,33 +1,31 @@
 function containsDuplicate(nums) {
 
 /****SOLUTION #1 ****/
-//   let emptyObject = {};
-//   for (let i = 0; i < nums.length; i++) {
-//     if (!emptyObject[nums[i]]) {
-//       emptyObject[nums[i]] = 0;
-//     }
-//     emptyObject[nums[i]]++;
-//   }
-
-//  for(keys in emptyObject){
-//    if(emptyObject[keys] >1){
-//      return true;
-//    }
-//  }
-//  return false;
-
-/****###END OF SOLUTION1 ###****/
-
-/****SOLUTION #2 ****/
-  for (let i=0; i<nums.length;i++){
-    if(nums.indexOf(nums[i]) > -1 && i){
-      return true;
+  let emptyObject = {};
+  let value =null;
+  for (let i = 0; i < nums.length; i++) {
+    if (!emptyObject[nums[i]]) {
+      emptyObject[nums[i]] = 0;
+       value =false;
     }
+    emptyObject[nums[i]]++;
+    value =true;
   }
-  return false;
+
+  return value;
+
+// /****###END OF SOLUTION1 ###****/
+
+// /****SOLUTION #2 ****/
+//   for (let i=0; i<nums.length;i++){
+//     if(nums.indexOf(nums[i]) > -1 && i){
+//       return true;
+//     }
+//   }
+//   return false;
 
 }
 
-let arr = [1,2,3,4];
+let arr = [1,2,3,];
 
 console.log(containsDuplicate(arr));
