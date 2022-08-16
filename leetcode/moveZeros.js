8e1a6ea3c6b42a6652df26a50592cf295843f7e8
AndyPark20 <apark1214@gmail.com>
2022-08-16 18:06:02 +0000

    reworking solution #1

--- leetcode/moveZeros.js
+++ leetcode/moveZeros.js
@@ -2,30 +2,30 @@
 
 function moveZero(arr){
   /***********Solution #1***********/
-  // let zeroArray=[];
-  // let nonZeroArray=[];
-  // let finalArray=[];
-  // for (let i=0;i<arr.length;i++){
-  //     if(arr[i]===0){
-  //         zeroArray.push(arr[i]);
-  //     }else if (arr[i] !==0){
-  //         nonZeroArray.push(arr[i]);
-  //     }        
-  // }
-  // nonZeroArray.sort((a,b)=>a-b);
-  // finalArray.push(...nonZeroArray, ...zeroArray);
+  let zeroArray=[];
+  let nonZeroArray=[];
+  let finalArray=[];
+  for (let i=0;i<arr.length;i++){
+      if(arr[i]===0){
+          zeroArray.push(arr[i]);
+      }else{
+          nonZeroArray.push(arr[i]);
+      }        
+  }
+  nonZeroArray.sort((a,b)=>a-b);
+  finalArray.push(...nonZeroArray, ...zeroArray);
 
-  // return finalArray;
+  return finalArray;
 
   /**********Solution #2 **********/
-  let highestValue =1;
-  let lowerValue =0;
-  let shuffled = [];
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]>=highestValue){
+  // let highestValue =1;
+  // let lowerValue =0;
+  // let shuffled = [];
+  // for(let i=0;i<arr.length;i++){
+  //   if(arr[i]>=highestValue && arr[i]<lowerValue){
       
-    }
-  }
+  //   }
+  // }
   
 }
 
