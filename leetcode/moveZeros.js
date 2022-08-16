9893267eaa279f12640461acc73e78163f724f8c
AndyPark20 <apark1214@gmail.com>
2022-08-16 14:12:16 +0000

    completed solution1

--- leetcode/moveZeros.js
+++ leetcode/moveZeros.js
@@ -1,8 +1,28 @@
-//Create two variables that will hold zero and nonzero values;
-//loop thru
 
-function moveZeros(){
-  let zeroArray =[];
-  let nonZeroArray=[];
-  for (let i=0;i<nums.length)
+
+function moveZero(arr){
+  /***********Solution #1***********/
+  // let zeroArray=[];
+  // let nonZeroArray=[];
+  // let finalArray=[];
+  // for (let i=0;i<arr.length;i++){
+  //     if(arr[i]===0){
+  //         zeroArray.push(arr[i]);
+  //     }else if (arr[i] !==0){
+  //         nonZeroArray.push(arr[i]);
+  //     }        
+  // }
+  // nonZeroArray.sort((a,b)=>a-b);
+  // finalArray.push(...nonZeroArray, ...zeroArray);
+
+  // return finalArray;
+
+  /**********Solution #2 **********/
+  
+  
 }
+
+let nums = [1,4,23,55,0,66,3,0];
+
+
+console.log(moveZero(nums));
