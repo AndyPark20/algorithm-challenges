de9364b764d79dbd5e2de04d593a9d06d33cdc89
AndyPark20 <apark1214@gmail.com>
2022-08-17 14:27:43 +0000

    working on solution 2

--- leetcode/moveZeros.js
+++ leetcode/moveZeros.js
@@ -2,34 +2,36 @@
 
 function moveZero(arr){
   /***********Solution #1***********/
-  let zeroArray=[];
-  let nonZeroArray=[];
-  let finalArray=[];
-  for (let i=0;i<arr.length;i++){
-      if(arr[i]===0){
-          zeroArray.push(arr[i]);
-      }else{
-          nonZeroArray.push(arr[i]);
-      }        
-  }
-  nonZeroArray.sort((a,b)=>a-b);
-  finalArray.push(...nonZeroArray, ...zeroArray);
+  // let zeroArray=[];
+  // let nonZeroArray=[];
+  // let finalArray=[];
+  // for (let i=0;i<arr.length;i++){
+  //     if(arr[i]===0){
+  //         zeroArray.push(arr[i]);
+  //     }else{
+  //         nonZeroArray.push(arr[i]);
+  //     }        
+  // }
+  // nonZeroArray.sort((a,b)=>a-b);
+  // finalArray.push(...nonZeroArray, ...zeroArray);
 
-  return finalArray;
+  // return finalArray;
 
   /**********Solution #2 **********/
-  // let highestValue =1;
-  // let lowerValue =0;
-  // let shuffled = [];
-  // for(let i=0;i<arr.length;i++){
-  //   if(arr[i]>=highestValue && arr[i]<lowerValue){
-      
-  //   }
-  // }
+  let lastNonZeroIndex=0;
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]!==0){
+      arr[lastNonZeroIndex] = arr[i];
+      lastNonZeroIndex++;
+    }
+  }
+  arr[arr.length-1] =0;
+  arr[arr.length-2]=0;
   
 }
 
 let nums = [1,4,23,55,0,66,3,0];
+
 
 
 console.log(moveZero(nums));
