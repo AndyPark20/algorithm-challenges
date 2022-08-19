bfd4a0b0476654a39754eb61b69b2bca7aeed747
AndyPark20 <apark1214@gmail.com>
2022-08-19 16:15:26 +0000

    re-working reomanNumeral

--- leetcode/romanNumeral.js
+++ leetcode/romanNumeral.js
@@ -1,41 +1,7 @@
 
 
 function guessNumber(string) {
-let romanNumerals={
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000
+
+
+
 }
-  //Create a variable called numbers that will keep count as we iterate thru converted array;
-  //create a variable called romanNumbers and store the converted string into array;
-  //loop thru romanNumbers, and check if the value at index is a property in the romanNumerals object;
-  //if it is a property, then add the number into numbers variable.
-
-
-  let numbers =0;
-  let romanNumbers =[...string];
-  for (let i=0;i<romanNumbers.length;i++){
-      if((romanNumbers[i] === 'I' && (romanNumbers[i + 1] === 'V' || romanNumbers[i + 1] === 'X'))){
-         numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
-         i++;
-      } else if ((romanNumbers[i] === 'X' && (romanNumbers[i + 1] === 'L' || romanNumbers[i + 1] === 'C'))){
-        numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
-        i ++;
-      } else if ((romanNumbers[i] === 'C' && (romanNumbers[i + 1] === 'D' || romanNumbers[i + 1] === 'M'))){
-       numbers+=romanNumerals[romanNumbers[i + 1]] - romanNumerals[romanNumbers[i]]
-         i++;
-      }else if(!romanNumbers[i]){
-        numbers;
-      }else{
-        numbers+=romanNumerals[romanNumbers[i]]
-      }
-  }
-  return numbers;
-}
-s = "MCMXCIV"
-
-console.log(guessNumber(s))
