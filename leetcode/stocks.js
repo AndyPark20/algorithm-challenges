44f9dfd8173e98c60c7fd0de0f1d0d345b719594
Andy Park <apark1214@gmail.com>
2022-08-23 08:29:55 -0700

    in progress

--- leetcode/stocks.js
+++ leetcode/stocks.js
@@ -1,1 +1,14 @@
-console.log('hello')
+
+
+
+function bestDayStocks(prices){
+  let priceByDays ={};
+  for(let i=0;i<prices.length;i++){
+
+  }
+}
+
+
+let prices=[7,1,5,3,6,4];
+
+console.log(bestDayStocks(prices))
