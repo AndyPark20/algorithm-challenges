8bb9785552f868270fd295526bd8e35ebc96908b
Andy Park <apark1214@gmail.com>
2022-08-25 08:25:16 -0700

    object cannot be used as timing and sequence is important

--- leetcode/stocks.js
+++ leetcode/stocks.js
@@ -2,15 +2,6 @@
 
 
 function bestDayStocks(prices){
-  let stockHistory={};
-
-  for(let i=0;i<prices.length;i++){
-    if(!stockHistory[i]){
-      stockHistory[i]=prices[i];
-    };
-  }
-
-  //Loop thru the object and find the smallest day with smallest value and subtract it from largest day with largest value
 
 }
 
