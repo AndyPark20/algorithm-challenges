b33d5346931eaee11811b96e7d075d3f8765802e
Andy Park <apark1214@gmail.com>
2022-08-25 08:34:26 -0700

    re-visit solution

--- leetcode/stocks.js
+++ leetcode/stocks.js
@@ -2,7 +2,14 @@
 
 
 function bestDayStocks(prices){
+  let maxValue=0;
 
+  for (let i=0;i<prices.length;i++){
+    for(let k=i+1;k<prices.length;k++){
+      maxValue = Math.max(maxValue, prices[k]-prices[i]);
+    }
+  }
+  return maxValue;
 }
 
 
