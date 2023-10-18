2213b37fb9eff8dc3fc07c65ded9ae02049e25d7
Andy Park <apark1214@gmail.com>
2023-10-17 17:26:13 -0700

    completed arraySum

--- hacker-rank/arraySum.js
+++ hacker-rank/arraySum.js
@@ -1,17 +1,9 @@
 
-function arraySum(array){
-  //the main output should return an integer summing up all the numbers inside the array
-  //It will take one single array as a parameter
-  //Create a variable that is going to store a number
-  // Loop thru the array and add the values into the variable that has been declared to store the number
-  //return variable after the loop has ended.
 
-  let arr=0;
-  for (let i=0;i<array.length;i++){
-    arr+=array[i];
-  }
-  return arr;
-
+const addSum = (arr) =>{
+  const sum = arr.reduce((accum, currentValue) => accum + currentValue)
+  return sum
 }
 
-console.log(arraySum([1, 2, 3, 4, 10, 11]))
+
+console.log(addSum([1,1,3]))
